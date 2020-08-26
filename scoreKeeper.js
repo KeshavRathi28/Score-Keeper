18a7cbfe5cab6fb39727b4954c23493d2ec43efb
Keshav Rathi <51473981+KeshavRathi28@users.noreply.github.com>
2020-08-26 16:52:08 +0530

    first commit: score keeper files

---
+++ scoreKeeper.js
@@ -0,0 +1,52 @@
+let p1Button = document.getElementById("p1");
+let p2Button = document.getElementById("p2");
+let p1Display = document.querySelector("#p1Display");
+let p2Display = document.querySelector("#p2Display");
+let resetButton = document.getElementById("reset");
+let numInput = document.querySelector("input[type=\"number\"]");
+let winningScoreDisplay = document.getElementById("display");
+
+let p1Score = 0;
+let p2Score = 0;
+let gameOver = false;
+let winningScore = 5;
+
+p1Button.addEventListener("click", function() {
+    if(!gameOver) {
+        p1Score++;
+        if(p1Score === winningScore) {
+            p1Display.classList.add("winner");
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;   
+    }
+});
+
+p2Button.addEventListener("click", function() {
+    if(!gameOver) {
+        p2Score++;
+        if(p2Score === winningScore) {
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;   
+    }
+});
+
+function reset() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+}
+
+resetButton.addEventListener("click", reset);
+
+numInput.addEventListener("change", function() {
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
+    reset();
+});
